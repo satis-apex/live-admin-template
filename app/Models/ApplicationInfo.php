288456b49938ae15cfb902ce9ef5021983df767e
satish maharjan <satish.maharjan55@gmail.com>
2022-09-20 13:17:32 +0545

    application info migration

---
+++ app/Models/ApplicationInfo.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class ApplicationInfo extends Authenticatable
+{
+	use HasApiTokens;
+	use HasFactory;
+	use Notifiable;
+
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array<int, string>
+	 */
+	protected $fillable = [
+		'title',
+		'email',
+		'address',
+	];
+
+	/**
+	 * The attributes that should be hidden for serialization.
+	 *
+	 * @var array<int, string>
+	 */
+	protected $hidden = [
+		'created_at',
+		'updated_at',
+	];
+}
