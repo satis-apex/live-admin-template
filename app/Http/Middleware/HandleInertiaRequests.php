c55291880a2d50693ea29ec2f6ab367767b02084
satish maharjan <satish.maharjan55@gmail.com>
2022-09-20 13:17:56 +0545

    inertia share application info globally

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\ApplicationInfo;
 use Illuminate\Http\Request;
 use Inertia\Middleware;
 use Tightenco\Ziggy\Ziggy;
@@ -43,6 +44,7 @@ class HandleInertiaRequests extends Middleware
                     'location' => $request->url(),
                 ]);
             },
+            'app_info' => ApplicationInfo::first(),
         ]);
     }
 }
