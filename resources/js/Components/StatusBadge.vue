abb12666cb9e73b17c3170e0fee2948d9f84feab
satish maharjan <satish.maharjan55@gmail.com>
2022-12-13 16:39:36 +0545

    chore: add dot badge component

---
+++ resources/js/Components/StatusBadge.vue
@@ -0,0 +1,39 @@
+<template>
+    <div
+        class="inline-flex items-center px-2 space-x-1 capitalize text-xs rounded-full"
+        :class="`status-badge-${type}`"
+    >
+        <div class="status-badge w-2 h-2 block rounded-full"></div>
+        <div><slot /></div>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    type: {
+        type: String,
+        default: "info",
+    },
+});
+</script>
+
+<style lang="scss" scoped>
+.status-badge {
+    background: currentColor;
+}
+.status-badge-success {
+    color: var(--el-color-success);
+}
+.status-badge-danger {
+    color: var(--el-color-danger);
+}
+.status-badge-primary {
+    color: var(--el-color-primary);
+}
+.status-badge-info {
+    color: var(--el-color-info);
+}
+.status-badge-warning {
+    color: var(--el-color-warning);
+}
+</style>
